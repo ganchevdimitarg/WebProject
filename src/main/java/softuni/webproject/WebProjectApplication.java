b1856225f0fe3accfaf44b12b0f686d2772804cc
ganchevdimitarg@gmail.com <ganchevdimitargeorgiev>
2019-11-10 16:38:17 +0200

    creation of the project

---
+++ src/main/java/softuni/webproject/WebProjectApplication.java
@@ -0,0 +1,12 @@
+package softuni.webproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebProjectApplication.class, args);
+    }
+}
